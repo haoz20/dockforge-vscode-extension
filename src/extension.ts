97905bec7f44413680d677193c2d2f5f98170f40
haoz20 <zhaonianhao2005@gmail.com>
2025-12-04 16:00:08 +0700

    remove: webview for the side bar

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,9 @@
-import { commands, ExtensionContext, window } from "vscode";
+import { commands, ExtensionContext } from "vscode";
 import { DockForgePanel } from "./panels/DockForgePanel";
 import { DockerHubPanel } from "./panels/DockerHubPanel";
-import { DockerfilesViewProvider } from "./views/DockerfilesViewProvider";
-
 
 export function activate(context: ExtensionContext) {
   console.log('DockForge extension is now active!');
-
-  // Register the webview view provider for Build Dockerfiles sidebar
-  const dockerfilesViewProvider = new DockerfilesViewProvider(context.extensionUri);
-  context.subscriptions.push(
-    window.registerWebviewViewProvider(
-      DockerfilesViewProvider.viewType,
-      dockerfilesViewProvider
-    )
-  );
 
   // Create the show DockForge command (opens the main panel)
   const showDockForgeCommand = commands.registerCommand("dockforge.showDockForge", (projectName?: string) => {
