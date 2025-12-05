4e5d270c07858357fd9b1fe7737eda13dd791331
haoz20 <zhaonianhao2005@gmail.com>
2025-12-05 21:02:38 +0700

    feat: implement Dockerfile tree data provider and item management

--- src/DockerfileTreeItem.ts
+++ src/DockerfileTreeItem.ts
@@ -4,11 +4,19 @@ import * as vscode from 'vscode';
 class DockerfileTreeItem extends vscode.TreeItem {
     constructor(
         public readonly label: string,
-        public readonly filePath: string
+        public readonly id: string
     ) {
-        super(label);
+        super(label, vscode.TreeItemCollapsibleState.None);
         this.contextValue = 'dockerfileItem';
-        this.iconPath = new vscode.ThemeIcon('file-code');
+        this.iconPath = new vscode.ThemeIcon('whale');
+        this.tooltip = `Dockerfile: ${label}`;
+        
+        // Add command to open when clicked
+        this.command = {
+            command: 'dockforge.openDockerfileBuilder',
+            title: 'Open Dockerfile',
+            arguments: [this]
+        };
     }
 }
 
